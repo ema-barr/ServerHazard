35221c9580cbd0ab4a892eecd627b0102998b69a
maccn <macc.nicola@gmail.com>
2016-12-29 12:32:44 +0100

    Gestione dei turni

--- Engine/src/it/uniba/hazard/engine/turn/EventTurn.java
+++ Engine/src/it/uniba/hazard/engine/turn/EventTurn.java
@@ -1,19 +1,22 @@
 package it.uniba.hazard.engine.turn;
 
+import it.uniba.hazard.engine.cards.EventCard;
+
 /**
  * Created by maccn on 25/12/2016.
  */
 
 public class EventTurn implements Turn {
 
-    private int event;  //attributo di tipo CartaEvento
+    private EventCard event;
 
-    public EventTurn (int e) {
+    public EventTurn (EventCard e) {
         event = e;
     }
+
     @Override
     public void startTurn() {
-        // event.execute(); attiva gli effetti della carta evento
+        event.executeAction();  // attiva gli effetti della carta evento
     }
 
 
