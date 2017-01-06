7fc7bdf9941180b344bc773d1dca56d1149a316f
maccn <macc.nicola@gmail.com>
2017-01-06 20:21:08 +0100

    Modifica dei parametri del metodo startTurn

--- Engine/src/it/uniba/hazard/engine/turn/Turn.java
+++ Engine/src/it/uniba/hazard/engine/turn/Turn.java
@@ -1,11 +1,13 @@
 package it.uniba.hazard.engine.turn;
 
+import it.uniba.hazard.engine.main.GameState;
+
 /**
  * Created by maccn on 25/12/2016.
  */
 public interface Turn {
 
-    public void startTurn ();
+    public void startTurn (GameState gameState);
 
 
 }
