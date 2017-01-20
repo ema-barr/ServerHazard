d4c385c8dc50763c3199cf01ad6dd50fd1bc160f
deBene92 <donato.debenedictis@libero.it>
2017-01-20 11:59:16 +0100

    Aggiunta Esecuzione Carta Evento Aggiungi Barriera

--- Engine/src/it/uniba/hazard/engine/cards/AddBlockade.java
+++ Engine/src/it/uniba/hazard/engine/cards/AddBlockade.java
@@ -1,6 +1,9 @@
 package it.uniba.hazard.engine.cards;
 
 import it.uniba.hazard.engine.main.GameState;
+import it.uniba.hazard.engine.map.Location;
+
+import java.util.Set;
 
 //Carta Evento: aggiunta barriera su una tratta.
 public class AddBlockade extends EventCard{
@@ -18,6 +21,27 @@ public class AddBlockade extends EventCard{
 
     @Override
     public void executeAction(GameState gameState) {
+        Set<Location> allLocations =  gameState.getMapLocations();
+        Location[] l = new Location[allLocations.size()-1];
+
+
+        int index = (int) (Math.random() * allLocations.size()-1);
+
+        //tutte le locazioni in un array
+        allLocations.toArray(l);
+
+        //preso primo nodo
+        Location l1 = l[index];
+
+        //prese locazioni adiacenti
+        Set<Location> l2 = gameState.getAdjacentLocations(l1);
+
+        for(Location l3: l2) {
+            gameState.block(l1, l3);
+        }
+    }
+
+    public void revertAction(GameState gameState) {
 
     }
 }
