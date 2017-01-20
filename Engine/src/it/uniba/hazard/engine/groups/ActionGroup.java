fadff38762b2cf8801cc24518207b7a3c0c833e5
ema-barr <emanuele.barracchia@hotmail.it>
2017-01-20 10:56:58 +0100

    Aggiunte le risorse usate nella classe ActionGroup

--- Engine/src/it/uniba/hazard/engine/groups/ActionGroup.java
+++ Engine/src/it/uniba/hazard/engine/groups/ActionGroup.java
@@ -2,26 +2,60 @@ package it.uniba.hazard.engine.groups;
 
 import it.uniba.hazard.engine.main.Emergency;
 import it.uniba.hazard.engine.main.Provisions;
+import it.uniba.hazard.engine.main.Resource;
 import it.uniba.hazard.engine.pawns.ActionPawn;
 
 import java.util.List;
 
 public class ActionGroup {
 
-    public List<Emergency> emergencyToBeSolved;
-    public Provisions provisions;
-    public ActionPawn actionPawn;
-    public String nameActionGroup;
+    private String objectID;
+    private List<Emergency> emergencyToBeSolved;
+    private List<Resource> usedRes;
+    private Provisions provisions;
+    private ActionPawn actionPawn;
+    private String nameActionGroup;
 
-    public ActionGroup(List<Emergency> emergencyToBeSolved, Provisions provisions, ActionPawn actionPawn, String nameActionGroup) {
+    public ActionGroup(List<Emergency> emergencyToBeSolved, List<Resource> usedRes, Provisions provisions, ActionPawn actionPawn, String nameActionGroup) {
+        this.objectID = this.getClass().getName() + "_" + nameActionGroup;
         this.emergencyToBeSolved = emergencyToBeSolved;
+        this.usedRes = usedRes;
         this.provisions = provisions;
         this.actionPawn = actionPawn;
         this.nameActionGroup = nameActionGroup;
     }
 
-    @Override
-    public String toString() {
+    public String getObjectID() {
+        return objectID;
+    }
+
+    public List<Emergency> getEmergencyToBeSolved() {
+        return emergencyToBeSolved;
+    }
+
+    public List<Resource> getUsedRes() {
+        return usedRes;
+    }
+
+    public Provisions getProvisions() {
+        return provisions;
+    }
+
+    public void setProvisions(Provisions provisions) {
+        this.provisions = provisions;
+    }
+
+    public ActionPawn getActionPawn() {
+        return actionPawn;
+    }
+
+    public String getNameActionGroup() {
         return nameActionGroup;
     }
+
+    @Override
+    public String toString() {
+        return getNameActionGroup();
+    }
+
 }
