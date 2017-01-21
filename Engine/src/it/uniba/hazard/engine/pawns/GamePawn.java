5b1ce7ce5d8669dbb8b7a5c3644465dc8b8061a3
aiovine <a.iovine1@studenti.uniba.it>
2017-01-21 19:22:47 +0100

    Aggiunto metodo getObjectID nelle interfacce GamePawn e PlayerPawn

--- Engine/src/it/uniba/hazard/engine/pawns/GamePawn.java
+++ Engine/src/it/uniba/hazard/engine/pawns/GamePawn.java
@@ -1,5 +1,5 @@
 package it.uniba.hazard.engine.pawns;
 
 public interface GamePawn {
-
+    public String getObjectID();
 }
--- Engine/src/it/uniba/hazard/engine/pawns/PlayerPawn.java
+++ Engine/src/it/uniba/hazard/engine/pawns/PlayerPawn.java
@@ -1,4 +1,5 @@
 package it.uniba.hazard.engine.pawns;
 
 public interface PlayerPawn extends GamePawn {
+    public String getObjectID();
 }
