692fd6dd85e12dd5f6c2b33488a52fe41aa57306
maccn <macc.nicola@gmail.com>
2017-01-28 13:07:23 +0100

    Fix vari

--- Engine/src/it/uniba/hazard/engine/turn/ActionTurn.java
+++ Engine/src/it/uniba/hazard/engine/turn/ActionTurn.java
@@ -1,6 +1,14 @@
 package it.uniba.hazard.engine.turn;
 
+import it.uniba.hazard.engine.cards.Card;
+import it.uniba.hazard.engine.groups.ActionGroup;
 import it.uniba.hazard.engine.main.GameState;
+import it.uniba.hazard.engine.map.Location;
+import it.uniba.hazard.engine.pawns.ActionPawn;
+import it.uniba.hazard.engine.pawns.GamePawn;
+
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by maccn on 25/12/2016.
@@ -11,10 +19,14 @@ import it.uniba.hazard.engine.main.GameState;
  */
 public class ActionTurn implements PlayerTurn {
 
-    private int player; // attributo di tipo GruppoProduzione
+    private ActionGroup player;
     private int pawns;
 
-    public ActionTurn (int pl, int pa) {
+    private List<Card> bonusCards;
+
+    private int numActions = 4;
+
+    public ActionTurn (ActionGroup pl, int pa) {
         player = pl;
         pawns = pa;
     }
@@ -23,27 +35,63 @@ public class ActionTurn implements PlayerTurn {
 
 
 
-    private void movePawn () {
+    private void movePawn (GameState gameState, String pawnStr, String currentLocationStr, String newLocationStr) {
+        Set<Location> ls = gameState.getMapLocations();
+        Location currentLocation = null;
+
+        for (Location l : ls) {
+            if (l.toString().equals(currentLocationStr)) {
+                currentLocation = l;
+            }
+        }
+
+        if (currentLocation != null) {
+            ActionPawn pawn = null;
+            Set<GamePawn> ps = gameState.getPawnsOnLocation(currentLocation);
+            for (GamePawn p : ps) {
+                ActionPawn temp = (ActionPawn) p;
+                if (temp.getObjectID().equals(pawnStr)) {
+                    pawn = temp;
+                }
+            }
+
+            if (pawn != null) {
+                ls = gameState.getAdjacentLocations(pawn);
+                Location newLocation = null;
+                for (Location l : ls) {
+                    if (l.toString().equals(newLocationStr)) {
+                        newLocation = l;
+                    }
+                }
+
+                if (newLocation != null) {
+                    //gameState.removePawn(pawn);
+                    gameState.movePawn(pawn, newLocation);
+                }
+            }
+        }
+    }
+
+    private void heal (GameState gameState) {
 
     }
 
-    private void heal () {
+    private void getResources (GameState gameState) {
 
     }
 
-    private void getResources () {
+    private void placeStronghold (GameState gameState) {
+        // TODO:
+        // Stronghold s = new Stronghold();
+        // gameState.placeStronghold(s);
+    }
+
+    // metodo per settare le carte bonus
+    private void setBonusCards (GameState gameState) {
 
     }
 
-    private void buildStronghold () {
-
-    }
-
-    private void getBonusCards () {
-
-    }
-
-    private void useBonusCard () {
+    private void useBonusCard (GameState gameState) {
 
     }
 
@@ -54,6 +102,26 @@ public class ActionTurn implements PlayerTurn {
 
     @Override
     public void runCommand(GameState gameState, String[] param) {
+        switch (param[0]) {
+            case "movePawn":
+                this.movePawn(gameState, param[1], param[2], param[3]);
+                break;
+            case "heal":
+                this.heal(gameState, param[1], param[2], param[3]);
+                break;
 
+            case "getResources":
+                this.getResources(gameState, param[1], param[2], param[3]);
+                break;
+            case "buildStronghold":
+                this.placeStronghold(gameState, param[1], param[2], param[3]);
+                break;
+            case "getBonusCards":
+                this.setBonusCards(gameState, param[1], param[2], param[3]);
+                break;
+            case "useBonusCard":
+                this.useBonusCard(gameState, param[1], param[2], param[3]);
+                break;
+        }
     }
 }
