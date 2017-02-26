80b0beeca524f7371c7b7bd1664102e47b87192d
aiovine <a.iovine1@studenti.uniba.it>
2017-02-26 12:35:44 +0100

    Implementata la serializzazione per la classe Blockade

--- Engine/src/it/uniba/hazard/engine/map/Blockade.java
+++ Engine/src/it/uniba/hazard/engine/map/Blockade.java
@@ -1,7 +1,9 @@
 package it.uniba.hazard.engine.map;
 
+import com.google.gson.*;
 import it.uniba.hazard.engine.exception.CannotCreateBlockadeException;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,5 +30,25 @@ public class Blockade {
 
     public boolean contains(Location l1, Location l2) {
         return locations.contains(l1) && locations.contains(l2);
+    }
+
+    public JsonElement toJson() {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(Blockade.class, new BlockadeSerializer());
+        return gsonBuilder.create().toJsonTree(this);
+    }
+
+    public class BlockadeSerializer implements JsonSerializer<Blockade> {
+
+        @Override
+        public JsonElement serialize(Blockade blockade, Type type, JsonSerializationContext jsonSerializationContext) {
+            JsonObject result = new JsonObject();
+            JsonArray locJson = new JsonArray();
+            for(Location l : locations) {
+                locJson.add(l.toString());
+            }
+            result.add("locations", locJson);
+            return result;
+        }
     }
 }
