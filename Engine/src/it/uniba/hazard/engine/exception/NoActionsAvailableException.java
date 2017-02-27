aa71fb55e620a25a892e792f0efb93932c080199
macc-n <macc.nicola@gmail.com>
2017-02-27 13:30:03 +0100

    Eccezione lanciata se si prova ad effettuare un'azione quando non si hanno più azioni disponibili

---
+++ Engine/src/it/uniba/hazard/engine/exception/NoActionsAvailableException.java
@@ -0,0 +1,10 @@
+package it.uniba.hazard.engine.exception;
+
+/**
+ * Created by maccn on 22/02/2017.
+ */
+public class NoActionsAvailableException extends RuntimeException {
+    public NoActionsAvailableException (String message) {
+        super(message);
+    }
+}
