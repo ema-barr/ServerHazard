852364521ad22af38df68509d19547cfb8944c48
macc-n <macc.nicola@gmail.com>
2017-03-03 17:07:38 +0100

    Interfaccia PlayerTurn

--- Engine/src/it/uniba/hazard/engine/turn/PlayerTurn.java
+++ Engine/src/it/uniba/hazard/engine/turn/PlayerTurn.java
@@ -2,11 +2,12 @@ package it.uniba.hazard.engine.turn;
 
 import it.uniba.hazard.engine.main.GameState;
 import it.uniba.hazard.engine.main.Turn;
+import it.uniba.hazard.engine.util.response.Response;
 
 /**
  * Created by maccn on 28/12/2016.
  */
 public interface PlayerTurn extends Turn {
 
-    public void runCommand (GameState gameState, String [] param);
+    public Response runCommand (GameState gameState, String [] param);
 }
