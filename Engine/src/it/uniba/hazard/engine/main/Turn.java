eeb571e642172d14a44fd91a170c046891c24bec
macc-n <macc.nicola@gmail.com>
2017-03-03 17:11:32 +0100

    executeTurn ora restituisce una Response

--- Engine/src/it/uniba/hazard/engine/main/Turn.java
+++ Engine/src/it/uniba/hazard/engine/main/Turn.java
@@ -1,11 +1,13 @@
 package it.uniba.hazard.engine.main;
 
 import com.google.gson.JsonElement;
+import it.uniba.hazard.engine.util.response.Response;
 
 /**
  * Created by andrea_iovine on 24/12/2016.
  */
 public interface Turn {
-    public void executeTurn(GameState state);
+
+    public Response executeTurn(GameState state);
     public JsonElement toJson();
 }
