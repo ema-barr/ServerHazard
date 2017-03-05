a876a43fbb26b0680dc3a144b4bee945afa26a4b
macc-n <macc.nicola@gmail.com>
2017-03-05 20:57:37 +0100

    Aggiunta la lista di carte produzione pescate

--- Engine/src/it/uniba/hazard/engine/util/response/production_turn/ProductionTurnExecuteTurnResponse.java
+++ Engine/src/it/uniba/hazard/engine/util/response/production_turn/ProductionTurnExecuteTurnResponse.java
@@ -1,8 +1,12 @@
 package it.uniba.hazard.engine.util.response.production_turn;
 
 import com.google.gson.JsonObject;
+
+import it.uniba.hazard.engine.cards.ProductionCard;
 import it.uniba.hazard.engine.groups.ProductionGroup;
 import it.uniba.hazard.engine.util.response.Response;
+
+import java.util.List;
 
 /**
  * Created by maccn on 03/03/2017.
@@ -11,11 +15,13 @@ public class ProductionTurnExecuteTurnResponse implements Response {
 
     private boolean success;
     private ProductionGroup productionGroup;
+    private List<ProductionCard> cards;
     private String logString;
 
-    public ProductionTurnExecuteTurnResponse (boolean success, ProductionGroup group) {
+    public ProductionTurnExecuteTurnResponse (boolean success, ProductionGroup group, List<ProductionCard> cards) {
         this.success = success;
         productionGroup = group;
+        this.cards = cards;
 
         if (success)
             logString = "Il gruppo " + productionGroup.toString() + " ha pescato le carte produzione.";
@@ -28,6 +34,7 @@ public class ProductionTurnExecuteTurnResponse implements Response {
         JsonObject res = new JsonObject();
         res.addProperty("success", success);
         res.addProperty("productionGroup", productionGroup.toString());
+        res.addProperty("cards", cards);
         res.addProperty("logString", logString);
         return res.toString();
     }
