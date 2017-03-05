599eb837f548b7241336976d878e6e43510a2b90
aiovine <a.iovine1@studenti.uniba.it>
2017-03-05 21:20:29 +0100

    Implementata la serializzazione di ProductionCard

--- Engine/src/it/uniba/hazard/engine/cards/ProductionCard.java
+++ Engine/src/it/uniba/hazard/engine/cards/ProductionCard.java
@@ -1,11 +1,14 @@
 package it.uniba.hazard.engine.cards;
 
+import com.google.gson.*;
 import it.uniba.hazard.engine.main.GameState;
 import it.uniba.hazard.engine.main.Resource;
 import it.uniba.hazard.engine.main.Turn;
 import it.uniba.hazard.engine.map.Location;
+import it.uniba.hazard.engine.turn.ProductionTurn;
 import it.uniba.hazard.engine.util.response.Response;
 
+import java.lang.reflect.Type;
 import java.util.Map;
 
 //Superclasse delle carte produzione
@@ -41,6 +44,31 @@ public class ProductionCard implements Card {
 
     public Map<Resource, Integer> getResource() {
         return resource;
+    }
+
+    public JsonElement toJson() {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(ProductionCard.class, new ProductionCardSerializer());
+        return gsonBuilder.create().toJsonTree(this);
+    }
+
+    public class ProductionCardSerializer implements JsonSerializer<ProductionCard> {
+
+        @Override
+        public JsonElement serialize(ProductionCard productionCard, Type type, JsonSerializationContext jsonSerializationContext) {
+            JsonObject response = new JsonObject();
+            response.addProperty("location", location.toString());
+            response.addProperty("locationID", location.getObjectID());
+            JsonArray resourcesJ = new JsonArray();
+            for(Resource r : resource.keySet()) {
+                JsonObject resJ = new JsonObject();
+                resJ.addProperty("resource", r.getNameResource());
+                resJ.addProperty("quantity", resource.get(r));
+                resourcesJ.add(resJ);
+            }
+            response.add("resources", resourcesJ);
+            return response;
+        }
     }
 
 
