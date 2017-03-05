d67c44021193db397d2f16091cdf6ef773d8a9f9
aiovine <a.iovine1@studenti.uniba.it>
2017-03-05 21:22:23 +0100

    Aggiunte le carte produzione

--- Engine/src/it/uniba/hazard/engine/util/response/production_turn/ProductionTurnExecuteTurnResponse.java
+++ Engine/src/it/uniba/hazard/engine/util/response/production_turn/ProductionTurnExecuteTurnResponse.java
@@ -1,8 +1,12 @@
 package it.uniba.hazard.engine.util.response.production_turn;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import it.uniba.hazard.engine.cards.ProductionCard;
 import it.uniba.hazard.engine.groups.ProductionGroup;
 import it.uniba.hazard.engine.util.response.Response;
+
+import java.util.List;
 
 /**
  * Created by maccn on 03/03/2017.
@@ -12,10 +16,12 @@ public class ProductionTurnExecuteTurnResponse implements Response {
     private boolean success;
     private ProductionGroup productionGroup;
     private String logString;
+    List<ProductionCard> productionCards;
 
-    public ProductionTurnExecuteTurnResponse (boolean success, ProductionGroup group) {
+    public ProductionTurnExecuteTurnResponse (boolean success, ProductionGroup group, List<ProductionCard> productionCards) {
         this.success = success;
         productionGroup = group;
+        this.productionCards = productionCards;
 
         if (success)
             logString = "Inizio turno del gruppo " + productionGroup.getObjectID() + " eseguito correttamente.";
@@ -29,6 +35,11 @@ public class ProductionTurnExecuteTurnResponse implements Response {
         res.addProperty("success", success);
         res.addProperty("productionGroup", productionGroup.getObjectID());
         res.addProperty("logString", logString);
+        JsonArray cardsJson = new JsonArray();
+        for(ProductionCard c : productionCards) {
+            cardsJson.add(c.toJson());
+        }
+        res.add("cards", cardsJson);
         return res.toString();
     }
 }
