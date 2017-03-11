ce9198cd2abb36e706fb1aa9685924a82aa5b2b6
ema-barr <emanuele.barracchia@hotmail.it>
2017-03-11 12:39:40 +0100

    Piccolo Fix e-mail git

--- Engine/src/it/uniba/hazard/engine/test/InitializationTest.java
+++ Engine/src/it/uniba/hazard/engine/test/InitializationTest.java
@@ -5,6 +5,7 @@ import it.uniba.hazard.engine.main.GameInitialization;
 public class InitializationTest {
     public static void main(String[] args){
         GameInitialization gi = new GameInitialization("strutturaxml.xml");
+
         gi.initialization();
     }
 }
