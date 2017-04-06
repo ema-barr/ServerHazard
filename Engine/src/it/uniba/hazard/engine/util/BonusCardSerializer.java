a4e6df9ac6051b1b7b8d3dcc7a4933e5efa3e89e
aiovine <a.iovine1@studenti.uniba.it>
2017-04-06 13:39:23 +0200

    Aggiunto il nome della carta bonus nella serializzazione

---
+++ Engine/src/it/uniba/hazard/engine/util/BonusCardSerializer.java
@@ -0,0 +1,23 @@
+package it.uniba.hazard.engine.util;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import it.uniba.hazard.engine.cards.BonusCard;
+import it.uniba.hazard.engine.cards.Card;
+
+import java.lang.reflect.Type;
+
+/**
+ * Created by isz_d on 06/04/2017.
+ */
+public class BonusCardSerializer implements JsonSerializer<BonusCard> {
+    @Override
+    public JsonElement serialize(BonusCard bonusCard, Type type, JsonSerializationContext jsonSerializationContext) {
+        JsonObject result = new JsonObject();
+        result.addProperty("objectID", bonusCard.getObjectID());
+        result.addProperty("name", bonusCard.getBonusType());
+        return result;
+    }
+}
