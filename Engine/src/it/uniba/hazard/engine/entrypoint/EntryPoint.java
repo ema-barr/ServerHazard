4bfcf17e940c844a4a67e00d3948c4ffc7b576fa
aiovine <a.iovine1@studenti.uniba.it>
2017-05-05 17:52:09 +0200

    Aggiunto entry point dell'applicazione

---
+++ Engine/src/it/uniba/hazard/engine/entrypoint/EntryPoint.java
@@ -0,0 +1,20 @@
+package it.uniba.hazard.engine.entrypoint;
+
+import it.uniba.hazard.engine.connection.ServerConnection;
+import it.uniba.hazard.engine.main.Game;
+import it.uniba.hazard.engine.main.GameInitialization;
+
+import java.net.URISyntaxException;
+
+/**
+ * Created by isz_d on 05/05/2017.
+ */
+public class EntryPoint {
+    public static void main(String[] args) throws URISyntaxException {
+        GameInitialization gi = new GameInitialization("strutturaxml.xml");
+        gi.initialization();
+        Game g = gi.getGame();
+        ServerConnection sc = new ServerConnection("http://localhost:6882", g);
+        sc.startConnection();
+    }
+}
